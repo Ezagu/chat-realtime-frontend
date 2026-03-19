7d071c234f5b996b518b5385bf51d27f1e768c47
Ezagu <agustincalpedm@gmail.com>
2026-03-19 03:15:16 -0300

    layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,120 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Check, CheckCheck, LogOut, Search, Send } from "lucide-react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className="h-screen w-full bg-background flex justify-center items-center text-text-primary">
+      <main className="w-full max-w-6xl h-2/3 bg-surface rounded-2xl grid grid-cols-[3fr_5fr]">
 
-      <div className="ticks"></div>
+        <aside className="min-w-0 bg-panel rounded-l-2xl border-r border-border">
+          <header className="flex w-full items-center border-b border-border px-4 py-2">
+            <div className="flex flex-col items-start grow truncate">
+              <h1 className="text-2xl font-medium text-start -mb-1">
+                Ezagu
+              </h1>
+              <div className="text-primary flex items-center gap-1">
+                <div className="size-1.5 bg-primary rounded-full"></div>
+                <p>online</p>
+              </div>
+            </div>
+            <button className="cursor-pointer hover:text-red-500 transition-colors">
+              <LogOut />
+            </button>
+          </header>
+          
+          <main className="py-2 px-4">
+            <label className="flex mb-5 mt-3 bg-card px-4 py-2 rounded-2xl has-[input:focus-within]:outline-primary has-[input:focus-within]:outline-2">
+              <Search className="text-text-secondary"/>
+              <input 
+                className="w-full placeholder:text-text-secondary pl-2 outline-0"
+                placeholder="Busca chats o usuarios"
+              />
+            </label>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+            <section className="flex gap-3 flex-col">
+              <article className="bg-card hover:bg-card-hover p-2 px-4 border-r-4 border-transparent rounded-xl cursor-pointer">
+                <header className="flex gap-2 items-baseline">
+                  <h2 className="text-lg font-medium truncate">dkiara</h2>
+                </header>
+                <main className="flex">
+                  <p className="grow truncate">HOLA ESTE ES MI MENSAJEeeeeeeeeeeeeeeeeeeeeeeeeeeeee</p>
+                  <span className="ml-2 -mr-2 text-text-secondary">19:53</span>
+                </main>
+              </article>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+              <article className="bg-card hover:bg-card-hover border-r-4 border-primary p-2 px-4 rounded-xl cursor-pointer">
+                <header className="flex gap-2 items-baseline">
+                  <h2 className="text-lg font-medium truncate">dkiara</h2>
+                </header>
+                <main className="flex">
+                  <p className="grow truncate">HOLA ESTE ES MI MENSAJE</p>
+                  <span className="ml-2 -mr-2 text-text-secondary">19:53</span>
+                </main>
+              </article>
+            </section>
+          </main>
+
+        </aside>
+
+        <main className="min-w-0 flex flex-col">
+          <header className="flex flex-col px-4 py-2 bg-panel rounded-tr-2xl ">
+            <div className="flex flex-col items-start grow truncate">
+              <h1 className="text-2xl font-medium text-start -mb-1">
+                dkiara
+              </h1>
+              <div className="text-text-secondary flex items-center gap-1">
+                <div className="size-1.5 bg-text-secondary rounded-full"></div>
+                <p>offline</p>
+              </div>
+            </div>
+          </header>
+
+          <section className="flex flex-col-reverse grow">
+            <article className="text-xl px-4 py-2 bg-primary-dark self-end rounded-2xl mb-3 mx-3 flex flex-col max-w-2/3">
+              <p>
+                Hola este es un mensaje muuuuuuuuuuuuuuuuuuuuuuuuuuuuuuy largooooooooooooooooooooooooo
+              </p>
+              <span className="flex items-center text-sm self-end gap-0.5">
+                <p >
+                  19:53
+                </p>
+                <div>
+                  <Check className="size-4"/>
+                </div>
+              </span>
+            </article>
+
+            <article className="text-xl px-4 py-2 bg-primary-dark self-end rounded-2xl mb-0.5 mx-3 flex flex-col max-w-2/3">
+              <p>
+                Hola este es un mensaje muuuuuuuuuuuuuuuuuuuuuuuuuuuuuuy largooooooooooooooooooooooooo
+              </p>
+              <span className="flex items-center gap-0.5 text-sm self-end">
+                <p >
+                  19:53
+                </p>
+                <div>
+                  <CheckCheck className="size-4"/>
+                </div>
+              </span>
+            </article>
+
+            <article className="text-xl px-4 py-2 bg-panel self-start rounded-2xl mb-2 mx-3 flex flex-col max-w-2/3">
+              <p>
+                Hola este es un mensaje
+              </p>
+              <span className="flex text-sm self-end">
+                <p className="text-text-secondary">
+                  19:53
+                </p>
+              </span>
+            </article>
+          </section>
+
+          <footer className="bg-panel rounded-br-2xl">
+            <form className="flex">
+              <input type="text" placeholder="Send a message..." className="bg-card text-lg px-4 py-1 grow m-2 rounded-4xl placeholder:text-text-secondary outline-0"/>
+              <button className="m-2 ml-0 size-10 flex items-center justify-center bg-primary-dark rounded-full hover:bg-primary-deeper cursor-pointer transition-colors hover:text-text-secondary">
+                <Send className="mr-0.5 mt-1"/>
+              </button>
+            </form>
+          </footer>
+
+        </main>
+
+      </main>
+    </div>
   )
 }
 
