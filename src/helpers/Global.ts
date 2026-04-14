160035076fb88123a6b0d9a63a46f95eca25eb6f
Ezagu <agustincalpedm@gmail.com>
2026-04-14 02:49:55 -0300

    updating the socket url

--- src/helpers/Global.ts
+++ src/helpers/Global.ts
@@ -1,1 +1,1 @@
-export const url = import.meta.env.VITE_API_URL || "http://localhost:3900/"
+export const url = import.meta.env.VITE_API_URL + '/' || "http://localhost:3900/"
--- src/services/websocket/socketService.ts
+++ src/services/websocket/socketService.ts
@@ -5,7 +5,7 @@ let socket: Socket | null = null;
 
 export const connect = () => {
   if(socket?.connected) return socket
-  socket = io('http://localhost:3900', {withCredentials: true})
+  socket = io(import.meta.env.VITE_API_URL || "http://localhost:3900", {withCredentials: true})
   return socket
 }
 
